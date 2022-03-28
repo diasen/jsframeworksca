0803086dffde32a4745bedd2bc1d5cc516943eae
Alexander Hansen <Alexander-hansen@hotmail.com>
2022-03-28 22:52:47 +0200

    updated login

--- pages/login.js
+++ pages/login.js
@@ -1,22 +1,22 @@
-// import React from 'react';
-// import { useSession, signIn, signOut } from 'next-auth/react';
+ import React from 'react';
+ import { useSession, signIn, signOut } from 'next-auth/react';
 
-// function Login() {
-// 	const { data: session } = useSession();
-// 	if (session) {
-// 		return (
-// 			<>
-// 				Signed in as {session.user.email} <br />
-// 				<button onClick={() => signOut()}>Sign out</button>
-// 			</>
-// 		);
-// 	}
-// 	return (
-// 		<>
-// 			Not signed in <br />
-// 			<button onClick={() => signIn()}>Sign in</button>
-// 		</>
-// 	);
-// }
+ function Login() {
+ 	const { data: session } = useSession();
+	if (session) {
+		return (
+ 			<>
+				Signed in as {session.user.email} <br />
+				<button onClick={() => signOut()}>Sign out</button>
+			</>
+		);
+	}
+ 	return (
+ 		<>
+			Not signed in <br />
+ 			<button onClick={() => signIn()}>Sign in</button>
+ 		</>
+	);
+ }
 
-// export default Login;
+ export default Login;
