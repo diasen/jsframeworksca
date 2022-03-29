00979f5468caa8e8f41791f5dc5571158dd93858
Alexander Hansen <Alexander-hansen@hotmail.com>
2022-03-29 23:48:11 +0200

    minor updates

--- pages/contact.js
+++ pages/contact.js
@@ -4,69 +4,88 @@ import * as Yup from 'yup';
 import Header from './components/header';
 
 function contact() {
-	const SignupSchema = Yup.object().shape({
-		firstName: Yup.string()
-			.min(2, 'Too Short, minimum 3 characters.')
-			.max(50, 'Too Long!')
-			.required('Required'),
-		lastName: Yup.string()
-			.min(2, 'Too Short, minimum 4 characters.')
-			.max(50, 'Too Long!')
-			.required('Required'),
-		email: Yup.string().email('Invalid email').required('Required'),
-		text: Yup.string().min(10, 'Too Short, minimum 10 characters.'),
-	});
+  const SignupSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .min(2, 'Too Short, minimum 3 characters.')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    lastName: Yup.string()
+      .min(2, 'Too Short, minimum 4 characters.')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    text: Yup.string().min(10, 'Too Short, minimum 10 characters.'),
+  });
 
-	return (
-		<div className="contactPage">
-			<Header/>
+  return (
+    <div className='contactPage'>
+      <Header />
 
-			<h1 className="h1Contact">Signup</h1>
-			<Formik
-				initialValues={{
-					firstName: '',
-					lastName: '',
-					email: '',
-					text: '',
-				}}
-				validationSchema={SignupSchema}
-				onSubmit={(values) => {
-					console.log(values);
-				}}>
-				{({ errors, touched }) => (
-					<Form className="mainForm">
-						<Field className="contactForm" name="firstName" placeholder="Firstname"/>
-						{errors.firstName && touched.firstName ? (
-							<div>{errors.firstName}</div>
-						) : null}
-						<Field className="contactForm" name="lastName" placeholder="Lastname" />
-						{errors.lastName && touched.lastName ? (
-							<div>{errors.lastName}</div>
-						) : null}
-						<Field className="contactForm" name="email" type="email" placeholder="Email" />
-						{errors.email && touched.email ? <div>{errors.email}</div> : null}
+      <h1 className='h1Contact'>Signup</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          text: '',
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Field
+              className='contactForm'
+              name='firstName'
+              placeholder='Firstname'
+            />
+            {errors.firstName && touched.firstName ? (
+              <div>{errors.firstName}</div>
+            ) : null}
+            <Field
+              className='contactForm'
+              name='lastName'
+              placeholder='Lastname'
+            />
+            {errors.lastName && touched.lastName ? (
+              <div>{errors.lastName}</div>
+            ) : null}
+            <Field
+              className='contactForm'
+              name='email'
+              type='email'
+              placeholder='Email'
+            />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
 
-						<Field className="contactForm" name="color" as="select" placeholder="Textfield">
-							<option value="hr">HR</option>
-							<option value="important">Important</option>
-							<option value="sallary">Sallary</option>
-						</Field>
+            <Field
+              className='contactForm'
+              name='color'
+              as='select'
+              placeholder='Textfield'
+            >
+              <option value='hr'>HR</option>
+              <option value='important'>Important</option>
+              <option value='sallary'>Sallary</option>
+            </Field>
 
-						<Field
-							placeholder="Message"
-							className="contactForm"
-							as="textarea"
-							name="text"
-							row="2"></Field>
-						{errors.text && touched.text ? <div>{errors.text}</div> : null}
-						<button className="submitButton" type="submit">
-							Submit
-						</button>
-					</Form>
-				)}
-			</Formik>
-		</div>
-	);
+            <Field
+              placeholder='Message'
+              className='contactForm'
+              as='textarea'
+              name='text'
+            ></Field>
+            {errors.text && touched.text ? <div>{errors.text}</div> : null}
+            <button className='submitButton' type='submit'>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
 export default contact;
